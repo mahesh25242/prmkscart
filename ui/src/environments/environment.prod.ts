3643937e4c2f031da77b75800e32cdd27cd4c8d0
Praveen <praveenknp@gmail.com>
2020-10-22 07:39:13 +0530

    UI fix

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   tinyMceApi: 'n6mryrdzt1pclqabsvpbvw6mtym3kj9gpwjguof89d2sv52q',
   baseUrl: 'https://cart/api/public',
   recaptchaKey: '6Ld2McEZAAAAABDjGwyJ63pD3ETCQkWCiZqEH39C',
-  lumenSecret: 'K6IlhS1oZBgxNQciIEtCoXzlHRGu0MefIkNkp68b',
+  lumenSecret: 'qsZyXU1bmZUWeCu2aUkF6lqAH8xxemstknjAThdP',
   client_id: 2 ,
   grant_type: "password",
-  siteAddress: 'http://localhost/cart',
+  siteAddress: 'http://localhost/prmkscart',
   gMapUrl: `http://maps.google.com/`,
   openstreetmap: `https://nominatim.openstreetmap.org`
 };
