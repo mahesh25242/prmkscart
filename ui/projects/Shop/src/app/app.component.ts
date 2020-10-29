f9e5ad96ff941f98f89c8a279c76919bacc4bbc0
mahesh K S <mahesh25242@gmail.com>
2020-10-29 09:32:54 +0530

    added navigation loading effect

--- ui/projects/Shop/src/app/app.component.ts
+++ ui/projects/Shop/src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+import Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +16,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '';
-
+  constructor(public router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          Notiflix.Loading.Arrows();
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          Notiflix.Loading.Remove();
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 }
