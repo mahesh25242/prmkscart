1b0198931e3f29828e5c3b51c393967eeb2b22de
mahesh K S <mahesh25242@gmail.com>
2020-11-22 12:39:40 +0530

    move cat icon to su

---
+++ ui/src/app/lib/constants/cat-icons.ts
@@ -0,0 +1,71 @@
+export const catIcons = [
+  {
+    name: 'Cake',
+    icon: 'cake'
+  },
+  {
+    name: 'Beverage',
+    icon: 'emoji_food_beverage'
+  },
+  {
+    name: 'Restaurant Menu',
+    icon: 'restaurant_menu'
+  },
+  {
+    name: 'Local Bar',
+    icon: 'local_bar'
+  },
+  {
+    name: 'Fast food',
+    icon: 'fastfood'
+  },
+  {
+    name: 'Cafe',
+    icon: 'local_cafe'
+  },
+  {
+    name: 'Pizza',
+    icon: 'local_pizza'
+  },
+  {
+    name: 'weekend',
+    icon: 'weekend'
+  },
+  {
+    name: 'Desktop',
+    icon: 'desktop_mac'
+  },
+  {
+    name: 'Keyboard',
+    icon: 'keyboard'
+  },{
+    name: 'Mouse',
+    icon: 'mouse'
+  },{
+    name: 'Watch',
+    icon: 'watch'
+  },{
+    name: 'Speaker',
+    icon: 'speaker'
+  },
+  {
+    name: 'Smartphone',
+    icon: 'smartphone'
+  },{
+    name: 'Electric Bike',
+    icon: 'electric_bike'
+  },
+  {
+    name: 'Electric Moped',
+    icon: 'electric_moped'
+  },{
+    name: 'Electric Car',
+    icon: 'electric_car'
+  }
+  ,{
+    name: 'Cycle',
+    icon: 'pedal_bike'
+  }
+
+]
+
