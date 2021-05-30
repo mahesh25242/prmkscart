1b3ba6a0920e802c8dd3d54e0d9ef1114636bc4d
Praveen <praveenknp@gmail.com>
2021-05-30 19:52:17 +0530

    Ui fix

--- ui/projects/Shop/src/app/cart-details/order-terms/order-terms.component.ts
+++ ui/projects/Shop/src/app/cart-details/order-terms/order-terms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-order-terms',
@@ -7,7 +9,7 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./order-terms.component.scss']
 })
 export class OrderTermsComponent implements OnInit {
-
+  environment = environment;
   constructor(public dialogRef: MatDialogRef<OrderTermsComponent>,) { }
 
   ngOnInit(): void {
