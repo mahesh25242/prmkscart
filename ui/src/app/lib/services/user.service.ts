12716c7dbdd936ed75b8eae8264f9c5b7873dc63
mahesh K S <mahesh25242@gmail.com>
2021-05-31 08:22:18 +0530

    c

--- ui/src/app/lib/services/user.service.ts
+++ ui/src/app/lib/services/user.service.ts
@@ -86,8 +86,11 @@ export class UserService {
     shareReplay({ bufferSize: 1, refCount: true }),
     catchError((err)=>{
      // console.log(x.status)
-     localStorage.removeItem('token');
+     if(err?.status == 401){
+      localStorage.removeItem('token');
       this.loggedUser.next(null);
+     }
+
       return throwError(err);
     })
     )
