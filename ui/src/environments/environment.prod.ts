9000b87534d9854336c6d2df8dda9fd77c2d6960
Mahesh <mahesh25242@gmail.com>
2025-05-14 16:47:44 +0530

    Update environment.prod.ts

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,22 +1,22 @@
 export const environment = {
   production: true,
-  tinyMceApi: 'n6mryrdzt1pclqabsvpbvw6mtym3kj9gpwjguof89d2sv52q',
-  baseUrl: 'https://api.breadnbutter.co.in/public/v1',
-  recaptchaKey: '6Ld2McEZAAAAABDjGwyJ63pD3ETCQkWCiZqEH39C',
-  lumenSecret: 'K6IlhS1oZBgxNQciIEtCoXzlHRGu0MefIkNkp68b',
+  tinyMceApi: 'asasasasas',
+  baseUrl: 'https://api.asaas.co.in/public/v1',
+  recaptchaKey: 'asaas',
+  lumenSecret: 'asasas',
   client_id: 2 ,
   grant_type: "password",
-  siteAddress: 'https://api.breadnbutter.co.in/public',
+  siteAddress: 'https://api.asasas.co.in/public',
   gMapUrl: `http://maps.google.com/`,
   openstreetmap: `https://nominatim.openstreetmap.org`,
   firebaseConfig : {
-    apiKey: "AIzaSyDo7xr0-tG22yldwClYGTm1cqCRdP1kpEI",
-    authDomain: "cart-5bd88.firebaseapp.com",
-    databaseURL: "https://cart-5bd88.firebaseio.com",
-    projectId: "cart-5bd88",
-    storageBucket: "cart-5bd88.appspot.com",
-    messagingSenderId: "971671236149",
-    appId: "1:971671236149:web:0e275f118ee00b18c98b7c",
-    measurementId: "G-R88KE3DEVR"
+    apiKey: "asaasas",
+    authDomain: "asasasas.assasas.com",
+    databaseURL: "https://cart-asdadasdasdasd.firebaseio.com",
+    projectId: "cart-asdsad",
+    storageBucket: "cart-asdsadasdasd.appspot.com",
+    messagingSenderId: "asdadasdadasd",
+    appId: "1:asdasdsad:web:asdasdasdasdasd",
+    measurementId: "G-Ssdfsdfsdfsf"
    }
 };
